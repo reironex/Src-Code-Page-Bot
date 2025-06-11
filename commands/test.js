018f52da03a9d0f797170db35c352c701b6f6065
kapev2 <159642874+kapev2@users.noreply.github.com>
2025-06-11 09:31:32 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -45,7 +45,15 @@ module.exports = {
           'user-agent': 'Mozilla/5.0 (Linux; Android 10)',
           'origin': 'https://www.blackbox.ai',
           'referer': `https://www.blackbox.ai/chat/${senderId}`,
-          'accept': '*/*'
+          'accept': '*/*',
+          'cookie': [
+            'render_app_version_affinity=dep-d149ps15pdvs73bbcn30',
+            'sessionId=336f68f2-86a9-4653-a5b5-b26e4c5f04d1',
+            'intercom-id-x55eda6t=aab4d07b-a6b8-4f3a-8177-c77e6bc7c4bb',
+            'intercom-device-id-x55eda6t=7e3922c6-d977-4e5a-a857-4376589d4bec',
+            '__Host-authjs.csrf-token=26ea109817a3a4670b17e20d689ddf15bd590f3d4c63e7e729b27680c548b7d5%7Cd7375ff6e111430dfc88d13771a179866975e5f822eb64e5d843f0cd2abb9820',
+            '__Secure-authjs.callback-url=https%3A%2F%2Fwww.blackbox.ai'
+          ].join('; ')
         }
       });
 
